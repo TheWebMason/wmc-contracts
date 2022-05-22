cd9d9f13191719544fd45ce705110e075239e6b9
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2022-05-22 11:31:14 +0300

    Create ITransferBatch.sol

---
+++ contracts/interfaces/ITransferBatch.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface ITransferBatch {
+    function transferBatch(address[] memory accounts, uint256[] memory amounts)
+        external
+        returns (bool);
+}

9bcae7c549369cae204a256cddd1da9dd2f627bf
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2022-06-12 01:47:40 +0300

    Create WebMasonCoinSafe.sol

---
+++ contracts/WebMasonCoinSafe.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact support@webmason.io
+contract WebMasonCoinSafe is Ownable {
+    constructor() {
+        //
+    }
+}

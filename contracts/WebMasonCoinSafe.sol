a71498689b949e9169a3a8fcfcdf0931ba3e922c
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2022-06-13 00:29:08 +0300

    up

--- contracts/WebMasonCoinSafe.sol
+++ contracts/WebMasonCoinSafe.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.14;
 
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "erc721a/contracts/ERC721A.sol";
 import "erc721a/contracts/extensions/IERC721ABurnable.sol";
 import "erc721a/contracts/extensions/ERC721AQueryable.sol";
 import "erc721a/contracts/extensions/ERC721AOwnersExplicit.sol";
 import "./utils/Recoverable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/IVesting.sol";
 import "./utils/ProxyRegistry.sol";
 
@@ -17,7 +19,8 @@ contract WebMasonCoinSafe is
     ERC721AQueryable,
     ERC721AOwnersExplicit,
     IERC721ABurnable,
-    Recoverable
+    Recoverable,
+    ReentrancyGuard
 {
     using Strings for uint256;
 
@@ -33,16 +36,28 @@ contract WebMasonCoinSafe is
     uint256 private constant _wmcAmount = 200_000 * 10**18;
 
     // WMC vesting parameters
+    uint256 private _initTime;
     uint256 public vestingStart = 1669852800; // 2022-12-01T00:00:00.000Z
     uint32 private constant _vesting = 5 * 12 * 30 * 24 * 60 * 60;
 
     // OpenSea
     address private _proxyRegistry;
 
-    constructor(address token_, address proxyRegistry_)
-        ERC721A("WebMasonCoin Safe", "WMC-SAFE")
-    {
+    // Mint
+    address public signer; // TODO
+    address public immutable wallet; // TODO
+    mapping(address => uint256) public mintNonce;
+
+    constructor(
+        address token_,
+        address wallet_,
+        address proxyRegistry_
+    ) ERC721A("WebMasonCoin Safe", "WMC-SAFE") {
+        _initTime = block.timestamp;
+
         WMC = token_;
+        wallet = wallet_;
+        signer = _msgSender();
         _proxyRegistry = proxyRegistry_;
     }
 
@@ -57,8 +72,15 @@ contract WebMasonCoinSafe is
         }
     }
 
-    function mint(uint256 quantity) external payable {
+    function mint(
+        uint256 quantity,
+        uint256 amount_,
+        uint256 nonce_,
+        bytes memory signature_
+    ) external payable nonReentrant {
         _checkTokenRestAmount(quantity);
+        _checkSignature(_msgSender(), quantity, amount_, nonce_, signature_);
+        _sendEth(wallet, amount_);
         _mint(_msgSender(), quantity);
     }
 
@@ -70,19 +92,127 @@ contract WebMasonCoinSafe is
         );
     }
 
-    function burn(uint256 tokenId) external {
+    function _sendEth(address recipient, uint256 amount) private {
+        (bool success, ) = recipient.call{value: amount}("");
+        require(success, "ETH_TRANSFER_FAILED");
+    }
+
+    // Signature
+    function setSigner(address newSigner) public onlyOwner {
+        signer = newSigner;
+    }
+
+    function signatureWallet(
+        address wallet_,
+        uint256 quantity_,
+        uint256 amount_,
+        uint256 nonce_,
+        bytes memory signature_
+    ) public pure returns (address) {
+        return
+            ECDSA.recover(
+                keccak256(abi.encode(wallet_, quantity_, amount_, nonce_)),
+                signature_
+            );
+    }
+
+    function _checkSignature(
+        address wallet_,
+        uint256 quantity_,
+        uint256 amount_,
+        uint256 nonce_,
+        bytes memory signature_
+    ) private {
+        require(
+            mintNonce[wallet_] < nonce_,
+            "Can not repeat a prior transaction!"
+        );
+        require(
+            signatureWallet(wallet_, quantity_, amount_, nonce_, signature_) ==
+                signer,
+            "Not authorized to mint"
+        );
+        mintNonce[wallet_] = nonce_;
+    }
+
+    // Burn
+    function burn(uint256 tokenId) public override {
         require(burnAllowed, "Burn not allowed");
         address tokenOwner = ownerOf(tokenId);
         _burn(tokenId, true);
-        IVesting(WMC).airdrop(
-            (vestingStart > block.timestamp)
-                ? uint32(vestingStart - block.timestamp)
-                : 0,
-            0,
-            _vesting,
-            tokenOwner,
-            uint96(_wmcAmount)
+
+        (uint64 tStart, , , uint32 tVesting, , , , ) = IVesting(WMC).vestingOf(
+            tokenOwner
         );
+        uint256 endTime = vestingStart + _vesting;
+
+        if (block.timestamp >= endTime) {
+            // If the vesting has already ended, then we do an airdrop without vesting.
+            IVesting(WMC).airdrop(0, 0, 0, tokenOwner, uint96(_wmcAmount));
+            return;
+        }
+
+        if (tStart == 0) {
+            // If vesting is not set.
+            if (vestingStart >= block.timestamp) {
+                // If time is less than vestingStart. Then we set up a new vesting.
+                IVesting(WMC).airdrop(
+                    uint32(vestingStart - block.timestamp),
+                    0,
+                    _vesting,
+                    tokenOwner,
+                    uint96(_wmcAmount)
+                );
+                return;
+            }
+            if (vestingStart < block.timestamp) {
+                // If time is greater than vestingStart.
+                // We increase the vested amount or set vesting.
+                uint256 remainingTime = 0;
+                uint256 locked = 0;
+
+                // Recalculate and set the vesting.
+                remainingTime = endTime - block.timestamp;
+                locked = (_wmcAmount * remainingTime) / _vesting;
+                // Set vesting
+                IVesting(WMC).airdrop(
+                    0,
+                    0,
+                    uint32(remainingTime),
+                    tokenOwner,
+                    uint96(locked)
+                );
+
+                // Airdrop of unlocked amount
+                IVesting(WMC).airdrop(
+                    0,
+                    0,
+                    0,
+                    tokenOwner,
+                    uint96(_wmcAmount - locked)
+                );
+                return;
+            }
+        } else {
+            /**
+             * If vesting is set.
+             * Option 1. tVesting is 5 years. The vesting was set before the tokens were unlocked.
+             * Then we add the quantity to the old quantity. Tokens will be unlocked automatically.
+             * Option 2. tVesting less than 5 years. The vesting was set after the tokens were unlocked.
+             * Then we airdrop the unlocked amount and add the remaining amount.
+             */
+            uint256 locked = _wmcAmount;
+            if (tVesting < _vesting) {
+                uint256 unlocked = (_wmcAmount * (_vesting - tVesting)) /
+                    _vesting;
+                locked = _wmcAmount - unlocked;
+                // Airdrop of unlocked amount.
+                IVesting(WMC).airdrop(0, 0, 0, tokenOwner, uint96(unlocked));
+            }
+            // Add the remaining amount.
+            IVesting(WMC).airdrop(0, 0, 1, tokenOwner, uint96(locked));
+            return;
+        }
     }
 
     function allowBurn() external onlyOwner {
@@ -91,7 +221,7 @@ contract WebMasonCoinSafe is
 
     function setVestingStart(uint256 newTime) external onlyOwner {
         require(
-            newTime < vestingStart,
+            newTime < vestingStart && newTime >= _initTime,
             "The new time must be less than the old vesting start time"
         );
         vestingStart = newTime;
--- migrations/200_deploy_token_safe.js
+++ migrations/200_deploy_token_safe.js
@@ -3,16 +3,18 @@ const Safe = artifacts.require('WebMasonCoinSafe');
 
 
 
-module.exports = async (deployer, network) => {
+module.exports = async (deployer, network, accounts) => {
+  let wallet = accounts[0];
   let proxyRegistry = '0x0000000000000000000000000000000000000000';
   if (network === 'development') {
     proxyRegistry = '0x0000000000000000000000000000000000000000';
   } else if (network === 'rinkeby') {
     proxyRegistry = '0xf57b2c51ded3a29e6891aba85459d600256cf317';
   } else if (network === 'mainnet') {
+    wallet = '0xaCE491eF8BC4d8F6735A4177560677F9592505e9'; // gnosis safe
     proxyRegistry = '0xa5409ec958c83c3f309868babaca7c86dcb077c1';
   }
 
-  const args = [Token.address, proxyRegistry];
+  const args = [Token.address, wallet, proxyRegistry];
   await deployer.deploy(Safe, ...args);
 };

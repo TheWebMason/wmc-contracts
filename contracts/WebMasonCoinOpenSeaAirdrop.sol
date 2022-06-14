91fc19c88c03a974ca44691120a699061facfc0e
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2022-06-14 18:54:08 +0300

    Update WebMasonCoinOpenSeaAirdrop.sol

--- contracts/WebMasonCoinOpenSeaAirdrop.sol
+++ contracts/WebMasonCoinOpenSeaAirdrop.sol
@@ -18,7 +18,7 @@ contract WebMasonCoinOpenSeaAirdrop is Ownable, Recoverable {
 
     // Airdrop
     bytes32 public merkleRoot;
-    uint256 public endTime;
+    uint256 public airdropEndTime;
 
     mapping(address => bool) public claimed;
     event AirdropClaimed(address indexed account, uint256 amount);
@@ -42,6 +42,78 @@ contract WebMasonCoinOpenSeaAirdrop is Ownable, Recoverable {
 
         claimed[account] = true;
         emit AirdropClaimed(account, amount);
+
+        (uint64 tStart, , , uint32 tVesting, , , , ) = IVesting(WMC).vestingOf(
+            account
+        );
+        uint256 endTime = vestingStart + _vesting;
+
+        if (block.timestamp >= endTime) {
+            // If the vesting has already ended, then we do an airdrop without vesting.
+            IVesting(WMC).airdrop(0, 0, 0, account, uint96(amount));
+            return;
+        }
+
+        if (tStart == 0) {
+            // If vesting is not set.
+            if (vestingStart >= block.timestamp) {
+                // If time is less than vestingStart. Then we set up a new vesting.
+                IVesting(WMC).airdrop(
+                    uint32(vestingStart - block.timestamp),
+                    0,
+                    _vesting,
+                    account,
+                    uint96(amount)
+                );
+                return;
+            }
+            if (vestingStart < block.timestamp) {
+                // If time is greater than vestingStart.
+                // We increase the vested amount or set vesting.
+                uint256 remainingTime = 0;
+                uint256 locked = 0;
+
+                // Recalculate and set the vesting.
+                remainingTime = endTime - block.timestamp;
+                locked = (amount * remainingTime) / _vesting;
+                // Set vesting
+                IVesting(WMC).airdrop(
+                    0,
+                    0,
+                    uint32(remainingTime),
+                    account,
+                    uint96(locked)
+                );
+
+                // Airdrop of unlocked amount
+                IVesting(WMC).airdrop(
+                    0,
+                    0,
+                    0,
+                    account,
+                    uint96(amount - locked)
+                );
+                return;
+            }
+        } else {
+            /**
+             * If vesting is set.
+             * Option 1. tVesting is 5 years. The vesting was set before the tokens were unlocked.
+             * Then we add the quantity to the old quantity. Tokens will be unlocked automatically.
+             * Option 2. tVesting less than 5 years. The vesting was set after the tokens were unlocked.
+             * Then we airdrop the unlocked amount and add the remaining amount.
+             */
+            uint256 locked = amount;
+            if (tVesting < _vesting) {
+                uint256 unlocked = (amount * (_vesting - tVesting)) / _vesting;
+                locked = amount - unlocked;
+                // Airdrop of unlocked amount.
+                IVesting(WMC).airdrop(0, 0, 0, account, uint96(unlocked));
+            }
+            // Add the remaining amount.
+            IVesting(WMC).airdrop(0, 0, 1, account, uint96(locked));
+            return;
+        }
     }
 
     function _leaf(address account, uint256 amount)
@@ -62,11 +134,11 @@ contract WebMasonCoinOpenSeaAirdrop is Ownable, Recoverable {
 
     function setRoot(bytes32 merkleRoot_, uint256 duration) external onlyOwner {
         merkleRoot = merkleRoot_;
-        endTime = block.timestamp + duration;
+        airdropEndTime = block.timestamp + duration;
     }
 
     function claim_rest_of_tokens_and_selfdestruct() external onlyOwner {
-        require(block.timestamp >= endTime, "Too early");
+        require(block.timestamp >= airdropEndTime, "Too early");
         require(
             IERC20(WMC).transfer(owner(), IERC20(WMC).balanceOf(address(this)))
         );
